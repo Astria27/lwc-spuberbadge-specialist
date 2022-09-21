772e86c6f7ac84c9b2062f2638be989b0c034952
NTELA Astria <astria.ntela@gmail.com>
2022-09-21 13:59:41 +0100

    Build the Boat Message Service Channel & create the boatSearch LWC

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+// Lightning Message Service and a message channel
+import { publish, MessageContext } from 'lightning/messageService';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c'
+
+export default class BoatSearch extends LightningElement {
+
+    
+}
