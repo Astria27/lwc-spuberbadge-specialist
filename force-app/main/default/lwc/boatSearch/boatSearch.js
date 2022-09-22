ae4029757d74acad92b9fc38b5c66b2f0c306fbc
NTELA Astria <astria.ntela@gmail.com>
2022-09-22 21:26:39 +0100

    Retrieve all metadata from the package

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,10 +1,3 @@
 import { LightningElement } from 'lwc';
 
-// Lightning Message Service and a message channel
-import { publish, MessageContext } from 'lightning/messageService';
-import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c'
-
-export default class BoatSearch extends LightningElement {
-
-    
-}
+export default class BoatSearch extends LightningElement {}
